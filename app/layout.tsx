f78ad56ae2dc9a168ec4b34c9ed8bdc400c7ac95
Fatih <contact@fatihcan.dev>
2024-10-15 11:58:30 +0300

    enable vercel's analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
+
 import { Providers } from './providers';
 import { ThemeProvider } from './components';
 import './globals.css';
@@ -21,6 +23,7 @@ export default function RootLayout({
           <ThemeProvider>{children}</ThemeProvider>
         </Providers>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
